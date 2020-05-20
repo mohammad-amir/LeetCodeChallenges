2b4e2bc04ab78b69e9cf462ebb4af5652cdbb210
mohammad-amir <amirbbd89@gmail.com>
2020-05-20 20:02:20 +0200

    new problem solved - refactoring

--- src/main/java/de/mohammadamir/LRUCache.java
+++ src/main/java/de/mohammadamir/LRUCache.java
@@ -75,15 +75,3 @@ public class LRUCache {
         }
     }
 }
-
-class DoubleLinkedListNode {
-    public int key;
-    public int value;
-    public DoubleLinkedListNode prev;
-    public DoubleLinkedListNode next;
-
-    public DoubleLinkedListNode(int key, int value){
-        this.key=key;
-        this.value=value;
-    }
-}
