e74054217acd0881b7a05963211b7fca7d7d4a90
mohammad-amir <amirbbd89@gmail.com>
2020-06-08 13:30:20 +0200

    new problem solved - Power of Two

--- src/main/java/de/mohammadamir/PowerOfTwo.java
+++ src/main/java/de/mohammadamir/PowerOfTwo.java
@@ -4,10 +4,6 @@ package de.mohammadamir;
 // https://leetcode.com/problems/power-of-two/
 public class PowerOfTwo {
     public boolean isPowerOfTwo(int n) {
-        if (n == 0) {
-            return false;
-        }
-
         while (n > 1 && n%2 == 0) {
             n = n/2;
         }
